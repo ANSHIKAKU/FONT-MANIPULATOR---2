00b7cc3d1d00a9099d0b87231f083b16d1f606de
ANSHIKAKU <92366994+ANSHIKAKU@users.noreply.github.com>
2022-07-02 07:34:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500)
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background('#5196e3')
+}
+
+function modelLoaded() {
+    console.log = ('PoseNet Is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = "+ noseX +" noseY =" + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        diffrence = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX =" + leftWristX +" rightWristX =" + rightWristX +" diffrence =" + diffrence);
+    }
+}
